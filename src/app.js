0d2c5f8a1c4dc21124ef60f0cb9db48264e1ded8
Alex Devero <deveroalex@gmail.com>
2017-07-18 09:16:59 +0200

    Add script for fetching Reddit api

--- src/app.js
+++ src/app.js
@@ -13,6 +13,8 @@ import env from './env';
 const app = remote.app;
 const appDir = jetpack.cwd(app.getAppPath());
 
+const settings = require('electron-settings');
+
 // Holy crap! This is browser window with HTML and stuff, but I can read
 // here files form disk like it's node.js! Welcome to Electron world :)
 const manifest = appDir.read('package.json', 'json');
@@ -23,8 +25,70 @@ const osMap = {
   linux: 'Linux',
 };
 
+settings.set('author', {
+  first: 'Alex',
+  last: 'Devero'
+});
+
+settings.get('author.first');
+
+settings.has('author.last');
+
 document.querySelector('#greet').innerHTML = greet();
 document.querySelector('#os').innerHTML = osMap[process.platform];
-document.querySelector('#author').innerHTML = manifest.author;
+document.querySelector('#author').innerHTML = `${settings.get('author.first')} ${settings.get('author.last')}`;
 document.querySelector('#env').innerHTML = env.name;
 document.querySelector('#electron-version').innerHTML = process.versions.electron;
+
+
+// Reddit fetch
+const menuEl = document.querySelector('.menu');
+const entriesEl = document.querySelector('.entries');
+const subredditBtn = document.querySelector('.subreddit-button');
+const subredditEl = document.querySelector('.subreddit-input');
+const subredditSelect = document.querySelector('.subreddit-select');
+
+const fetchSubreddit = (url) => {
+  if (url) {
+    fetch('https://www.reddit.com/r/' + url + '.json').then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      let links = '';
+
+      for (let i = 0; i < json.data.children.length; i++) {
+        links += '<li><a href="' + json.data.children[i].data.url + '">' +
+          json.data.children[i].data.url + '</a></li>';
+      }
+
+      entriesEl.innerHTML = '';
+      entriesEl.innerHTML = '<ul>' + links + '</ul>';
+    });
+  }
+}
+
+subredditBtn.addEventListener('click', (e) => {
+  let subreddit = subredditEl.value;
+  let subredditsByTopicUrl = `https://www.reddit.com/api/subreddits_by_topic.json?query=${subreddit}`;
+
+  fetch(subredditsByTopicUrl).then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    const select = document.createElement('select');
+    let links = '<option value="empty" selected disabled>Select one topic</option>';
+
+    for (let k = 0; k < json.length; k++) {
+      links += '<option value="' + json[k].name + '">' + json[k].name +
+        '</option>';
+    }
+
+    subredditSelect.innerHTML = links;
+
+    subredditSelect.classList.remove('hidden');
+
+    subredditSelect.addEventListener('change', (e) => {
+      fetchSubreddit(e.target.value);
+    });
+
+    // menuEl.appendChild(select);
+  })
+});
